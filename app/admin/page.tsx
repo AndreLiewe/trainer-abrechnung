e1a1e60304a211494fbcc472c1a1a667e25bf96a
AndreLiewe <webmaster@bsv-wahrsow.de>
2025-05-06 22:57:47 +0200

    dayjs eingefügt

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useEffect, useState, useCallback } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { Card, CardContent } from "@/components/ui/card";
