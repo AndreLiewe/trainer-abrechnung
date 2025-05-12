1061fcb3472cc88b6cc284017ccfed75ebc7f8ef
Andre Liewe <webmaster@bsv-wahrsow.de>
2025-05-12 12:20:02 +0200

    key geändert

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://szuxymwbyktjtrfvvmhm.supabase.co"; // <- Deine Project URL
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6dXh5bXdieWt0anRyZnZ2bWhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0NjAyMDIsImV4cCI6MjA1OTAzNjIwMn0.rds4MGTbwX5FpGfP7gRbAgGAGnLa-wpeXJ3sud1Neiw";   // <- Dein anon key
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
