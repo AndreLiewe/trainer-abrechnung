55c5cda603c2e32bb19b05e3fdb02e6e3fae58b0
Andre Liewe <webmaster@bsv-wahrsow.de>
2025-05-12 14:50:25 +0200

    ich werde verrückt

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,7 +2,7 @@
 
 export const dynamic = 'force-dynamic';
 
-export async function POST(req: Request) {
+export async function POST(_req: Request) {
   return new Response(JSON.stringify({ ok: true, message: "Die API funktioniert 🎉" }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
