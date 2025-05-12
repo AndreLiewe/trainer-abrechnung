9bf351a6f5e113e783f2da3e364cec513a2d1cc6
Andre Liewe <webmaster@bsv-wahrsow.de>
2025-05-12 14:53:04 +0200

    verzweiflung

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,10 @@
 // app/api/erzeuge-abrechnung/route.ts
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 export const dynamic = 'force-dynamic';
 
-export async function POST(_req: Request) {
+export async function POST(req: Request) {
   return new Response(JSON.stringify({ ok: true, message: "Die API funktioniert 🎉" }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
