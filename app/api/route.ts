8f1d540f68adf73887f3643f3d05c1445e923f85
Andre Liewe <webmaster@bsv-wahrsow.de>
2025-05-12 15:05:56 +0200

    kotz

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,12 @@
-// app/api/erzeuge-abrechnung/route.ts
-
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 export const dynamic = 'force-dynamic';
 
 export async function POST(req: Request) {
-  return new Response(JSON.stringify({ ok: true, message: "Die API funktioniert 🎉" }), {
+  return new Response(JSON.stringify({
+    ok: true,
+    url: "https://example.com/test.pdf"
+  }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   });
